d153fd6b477cfc3fcdf57022a7a50677ba7f4d60
PxViet <xuanviet0299@gmail.com>
2022-12-13 15:11:08 +0700

    #13 string in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,10 +1,9 @@
-const precedence = 3 * 3 - (20 % 3) - 7 * 2 + 5 * 6;
-const number = "This is string";
+const myString = 'Why you wanna do that?';
+const myAnswer = 'Just i like it';
 
-if (precedence === 18) {
-  console.log(`precedence = ${precedence}`);
-} else if (precedence > 18) {
-  console.log("precedence lager 18");
-} else {
-  console.log("precedence smaller than 18");
-}
+const myConversation = `${myString} 
+${myAnswer}
+`;
+
+console.log(myConversation);
+console.log("length of variable 'myConversation' is: ",myConversation.length );
