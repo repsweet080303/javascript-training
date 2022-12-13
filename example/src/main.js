dfa28158c7319c1e8bc474da96bbc02a5aa0daef
PxViet <xuanviet0299@gmail.com>
2022-12-13 11:50:56 +0700

    #11 comparisons in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,4 +1,5 @@
 const precedence = 3 * 3 - (20 % 3) - 7 * 2 + 5 * 5;
-const number = 20 % 3;
-alert(precedence);
-console.log(number);
+const number = "This is string";
+
+const result = precedence === number;
+alert(result);
