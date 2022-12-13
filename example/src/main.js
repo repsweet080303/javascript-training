bd6ad8dbbeb2a191d59cd930accfe6784ad2373c
PxViet <xuanviet0299@gmail.com>
2022-12-13 16:43:41 +0700

    #14 array in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,9 +1,23 @@
-const myString = 'Why you wanna do that?';
-const myAnswer = 'Just i like it';
+const a = ["this", "is"];
+const b = ["my", "cat"];
+const c = [...a, ...b];
 
-const myConversation = `${myString} 
-${myAnswer}
-`;
+const myMatrix = [
+  [2, 3, 6],
+  [3, 1, 9],
+  [7, 7, 7],
+  [5, 2, 8],
+];
 
-console.log(myConversation);
-console.log("length of variable 'myConversation' is: ",myConversation.length );
+const indexArray = myMatrix[1][2];
+
+const myArray = Array(7).fill("array", 2);
+myArray.push(77, 78);
+myArray.unshift(22, 0);
+myMatrix[1].push(2);
+
+console.log(myArray.shift());
+console.log(myMatrix);
+console.log(indexArray);
+console.log(c.includes("this", 1));
+console.log(c.findIndex((element) => element === "cat"));
