5467e64a9199a19e1a225acea56156a6361a4006
PxViet <xuanviet0299@gmail.com>
2022-12-13 14:06:37 +0700

    #12 precedence and if else conditional

--- example/src/main.js
+++ example/src/main.js
@@ -1,5 +1,10 @@
-const precedence = 3 * 3 - (20 % 3) - 7 * 2 + 5 * 5;
+const precedence = 3 * 3 - (20 % 3) - 7 * 2 + 5 * 6;
 const number = "This is string";
 
-const result = precedence === number;
-alert(result);
+if (precedence === 18) {
+  console.log(`precedence = ${precedence}`);
+} else if (precedence > 18) {
+  console.log("precedence lager 18");
+} else {
+  console.log("precedence smaller than 18");
+}
