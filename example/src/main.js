d6c8aa240a70f445799a0f1ae48edaa0e09f2e06
PxViet <xuanviet0299@gmail.com>
2022-12-14 09:10:13 +0700

    #15 while loop in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,23 +1,9 @@
-const a = ["this", "is"];
-const b = ["my", "cat"];
-const c = [...a, ...b];
+let text = "";
+let i = 0;
+while (i < 10) {
+  if (i === 3) break;
+  text += "This is " + i + " <br> ";
+  i++;
+}
 
-const myMatrix = [
-  [2, 3, 6],
-  [3, 1, 9],
-  [7, 7, 7],
-  [5, 2, 8],
-];
-
-const indexArray = myMatrix[1][2];
-
-const myArray = Array(7).fill("array", 2);
-myArray.push(77, 78);
-myArray.unshift(22, 0);
-myMatrix[1].push(2);
-
-console.log(myArray.shift());
-console.log(myMatrix);
-console.log(indexArray);
-console.log(c.includes("this", 1));
-console.log(c.findIndex((element) => element === "cat"));
+document.getElementById("demo").innerHTML = text;
