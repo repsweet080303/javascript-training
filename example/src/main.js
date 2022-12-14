033e02e5b7fc7dbad9e4fd984bbbc335bd951514
PxViet <xuanviet0299@gmail.com>
2022-12-14 09:58:32 +0700

    #15 for...of, for...in loop in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,5 +1,9 @@
 const myList = ["Ferrari", "Lamborghini", "Porsche"];
 
-for (var i = 0; i < myList.length; i++) {
-  console.log("car " + (1 + i) + " is " + myList[i]);
+for (const value of myList) {
+  console.log(value);
+}
+
+for (const value in myList) {
+  console.log(value);
 }
