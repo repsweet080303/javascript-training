274f149b69a6aa602409e987ab8e7cd86185a159
PxViet <xuanviet0299@gmail.com>
2022-12-14 17:10:22 +0700

    #20 object method in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,33 +1,14 @@
-function Girl(name, tall, heavy, gender) {
-  this.name = name;
-  this.tall = tall;
-  this.heavy = heavy;
-  this.gender = "female";
-}
-
-const myGirl = new Girl("Linh", 154, 47);
-
-console.log(myGirl);
-
-const myFriend = {
-  name: "Xuho",
-  age: 23,
-  character: "Kind",
-  body: "Tiny",
-  "space#speakers": "SP",
+const obj = {
+  name: "Duong",
+  nick: "Hoa",
+  method() {
+    return {
+      age: "20",
+      newMethod: () => {
+        console.log(obj.method().age);
+      },
+    };
+  },
 };
 
-console.log(myFriend);
-
-const otherFriend = myFriend;
-otherFriend.name = "Ha";
-otherFriend.age = 22;
-otherFriend["space#speakers"] = "MID";
-otherFriend.work = "Design";
-console.log(otherFriend);
-
-const anotherFriend = otherFriend;
-anotherFriend.name = "Cham";
-delete anotherFriend["space#speakers"];
-console.log(anotherFriend);
-
+obj.method().newMethod();
