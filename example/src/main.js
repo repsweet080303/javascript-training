c8c7045f9df286026b84356a7fd6d71a8481190e
PxViet <xuanviet0299@gmail.com>
2022-12-15 08:58:21 +0700

    #20 object method with for loop in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,14 +1,17 @@
-const obj = {
-  name: "Duong",
-  nick: "Hoa",
-  method() {
-    return {
-      age: "20",
-      newMethod: () => {
-        console.log(obj.method().age);
-      },
-    };
+const myMotobike = {
+  name: "Sirious",
+  color: "Silver and blue",
+  heavy: 105,
+  description: function (param) {
+    for (var i = 0; i < param.length; i++) {
+      console.log(
+        `My motobike name ${this.name} and it have color ${this.color}, ${param[i]}`
+      );
+    }
   },
 };
 
-obj.method().newMethod();
+myMotobike.description([
+  "I love this moto so much",
+  "This i my present of my mom",
+]);
