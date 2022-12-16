d5a65a595e6d92e6eb936218e2b1322aad0aa1f3
PxViet <xuanviet0299@gmail.com>
2022-12-16 17:08:35 +0700

    #24 promise in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,5 +1,58 @@
-console.log(1);
-console.log(2);
-setTimeout(() => console.log(4), 3000);
-setTimeout(() => console.log(3), 2000);
-console.log(5);
+const promise = new Promise(function (resolve, reject) {
+  resolve();
+});
+
+promise
+  .then(function () {
+    return 1;
+  })
+  .then(function (data) {
+    console.log(data);
+    return ++data;
+  })
+  .then(function (data) {
+    setTimeout(function () {
+      console.log(data);
+      return data++;
+    }, 3000);
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function () {
+    console.log("failure");
+  })
+  .finally(function () {
+    console.log("done");
+  });
+
+function waiting(milisec) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, milisec);
+  });
+}
+
+waiting(2000)
+  .then(function () {
+    console.log("done");
+    return new Promise((resolve, reject) => {
+      setTimeout(reject("Kabku"), 2000);
+    });
+  })
+  .then(function () {
+    console.log("donne");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+const myArray = [1, 2, 3, 4, 5];
+
+myArray.forEach(function (item) {
+  setTimeout(function () {
+    for (let i = 0; i <= 77; i++) {
+      console.log(i);
+    }
+  }, 5000);
+  console.log(item);
+});
