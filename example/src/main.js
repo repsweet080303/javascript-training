c0dcd8d95dc68be7efa4ecd11a4d48ffe735d1a0
PxViet <xuanviet0299@gmail.com>
2022-12-19 15:39:08 +0700

    #25 async await in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,73 +1,13 @@
-function handOut(callback) {
-  console.log("handOut");
-  callback();
+let dataApi = [];
+async function fecthData() {
+  try {
+    const response = await fetch("https://api.github.com/users/octocat");
+    const data = await response.json();
+    console.log(data.avatar_url);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function handIn(cb) {
-  setTimeout(() => {
-    console.log("handIn");
-    cb();
-  }, 2000);
-}
-
-function handOn(callback) {
-  console.log("handOn");
-}
-
-handOut(handOn);
-handIn(handOn);
-handOn(handOut);
-
-const promise = new Promise((resolve, reject) => {
-  resolve("Value of promise");
-});
-
-promise
-  .then((value) => {
-    console.log(value);
-    return value + "is chain";
-  })
-  .then((value) => {
-    console.log(value);
-  });
-
-function getData(response) {
-  return new Promise((resolve, reject) => {
-    if (response) {
-      resolve([
-        {
-          name: "Hung",
-          age: 22,
-        },
-        {
-          name: "Loan",
-          age: 21,
-        },
-        {
-          name: "Doan",
-          age: 23,
-        },
-      ]);
-    } else reject("Failed to resolve");
-  });
-}
-
-getData(0)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-fetch("https://api.github.com/users/octocat")
-  .then((response) => {
-    console.log(response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+fecthData();
