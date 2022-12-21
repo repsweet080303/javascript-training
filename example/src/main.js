6dd33888e25ac989de266814e4af418aeee663f1
PxViet <xuanviet0299@gmail.com>
2022-12-21 09:56:12 +0700

    5 async await with macroTask example

--- example/src/main.js
+++ example/src/main.js
@@ -1,8 +1,15 @@
-function getData() {
-  if (true) {
-    let data;
-    data = "some data";
-    console.log(data);
-  }
+async function getName(name) {
+  // if (name === "Inosuke") {
+  //   setTimeout(() => console.log(name), 2000);
+  // }
+  return name;
 }
-getData();
+
+const testSpeed = async () => {
+  const characterOne = await getName("Nezuko");
+  const characterTwo = await setTimeout(() => console.log("Inosuke"), 2000);
+  const characterThree = await getName("Zenitsu");
+  return [characterOne, characterTwo, characterThree];
+};
+
+testSpeed().then((res) => console.log(res));
