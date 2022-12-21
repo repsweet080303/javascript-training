a75aa719fabc0e96f7dfe8cb53aef0f3d55ac16d
PxViet <xuanviet0299@gmail.com>
2022-12-21 09:05:30 +0700

    26 variables scope in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,15 +1,8 @@
-let dataApi = [];
-async function fecthData() {
-  try {
-    const response = await fetch("https://api.github.com/users/octocat");
-    const data = await response.json();
-    console.log(data.avatar_url);
-    return data;
-  } catch (error) {
-    console.log(error);
+function getData() {
+  if (true) {
+    let data;
+    data = "some data";
+    console.log(data);
   }
 }
-
-fecthData().then(function (data) {
-  console.log(data.login);
-});
+getData();
