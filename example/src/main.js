d6570b428a4ee2e753ef64fa1fd3bb4ad604a2d5
PxViet <xuanviet0299@gmail.com>
2022-12-21 16:43:43 +0700

    #31 Closure in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,25 +1,54 @@
-const getUsers = async () => {
-  const data = await fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => {
-      return true;
-    })
-    .catch((err) => {
-      console.log(err);
-      return 132;
-    });
-  console.log(data);
-};
+function createCounter() {
+  let counter = 0;
 
-getUsers();
+  function increaseCounter() {
+    return ++counter;
+  }
 
-// res.then((data) => {
-//   console.log(data);
-// });
-
-try {
-  // fetch("https://jsonplaceholder.typicode.com/users");
-  parseInt("xddddd");
-  console.log(x);
-} catch (error) {
-  throw new Error("123321");
+  return increaseCounter;
 }
+
+const counter = createCounter();
+
+console.log(counter());
+console.log(counter());
+
+// ==========================================
+
+function debug(namespace) {
+  function logger(message) {
+    console.log(`${namespace} ${message}`);
+  }
+
+  return logger;
+}
+
+const loggerInfo = debug("Info");
+
+loggerInfo("Starting login");
+loggerInfo("Accuracy account");
+loggerInfo("Completed accuracy account");
+
+const loggerError = debug("Error");
+
+loggerError("Starting login");
+loggerError("Accuracy account failed");
+loggerError("Log out");
+
+function createCar() {
+  let cars = [];
+
+  return {
+    getCar(car) {
+      cars.push(car);
+    },
+    showCar() {
+      console.log(cars);
+    },
+  };
+}
+const myCar = createCar();
+myCar.showCar();
+myCar.getCar("BMW");
+myCar.getCar("Mercedes");
+myCar.showCar();
