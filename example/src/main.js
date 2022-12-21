b49d53413dff875977c36799eb252a2fa57865f6
PxViet <xuanviet0299@gmail.com>
2022-12-21 13:57:59 +0700

    #25 Async await in example in JavaScript

--- example/src/main.js
+++ example/src/main.js
@@ -1,45 +1,25 @@
-const myObject = {
-  name: "Zenitsu",
-  skill: "Flash storm",
-  color: "Yellow",
+const getUsers = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => {
+      return true;
+    })
+    .catch((err) => {
+      console.log(err);
+      return 132;
+    });
+  console.log(data);
 };
 
-for (let [key, value] of Object.entries(myObject)) {
-  console.log(`${key}: ${value}`);
+getUsers();
+
+// res.then((data) => {
+//   console.log(data);
+// });
+
+try {
+  // fetch("https://jsonplaceholder.typicode.com/users");
+  parseInt("xddddd");
+  console.log(x);
+} catch (error) {
+  throw new Error("123321");
 }
-
-const yourObject = {
-  ...myObject,
-  name: "Tanjiro",
-  skill: "Dragon fire",
-  other: "Kind",
-};
-
-function display({ name, skill, color, other }) {
-  console.log(name);
-  console.log(skill);
-  console.log(color);
-  console.log(other);
-}
-
-display(myObject);
-display(yourObject);
-
-const stt = [23, 2, 3, 4];
-const sst2 = {
-  0: [234],
-};
-
-sst2[0] = 234;
-
-function a() {
-  const i = 1;
-  return () => {
-    console.log(i);
-  };
-}
-
-const x = a();
-x();
-
-
