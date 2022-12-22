2925ae47b1af1bde053625304b275bf7b8a744b8
PxViet <xuanviet0299@gmail.com>
2022-12-22 13:47:16 +0700

    #33 understand DOM tree and nodes

--- example/src/main.js
+++ example/src/main.js
@@ -1,32 +1,7 @@
-let myArray = ["Sam", "Loan"];
+let button = document.getElementById("changeBackground");
 
-const [name, subname] = myArray;
-
-console.log(name, subname);
-
-myArray = ["Sam", "Smitt"];
-
-console.log(myArray);
-console.log(name, subname);
-
-const [firstName, lastName] = "Johnny Dang".split(" ");
-console.log(firstName, lastName);
-
-const myObject = {
-  name: {
-    firstName: "Tanjiro",
-    lastName: "Komado",
-  },
-  skill: "Yellow storm",
-  desc: "Little boy unlucky in training",
-};
-
-let {
-  name: { firstName: firstNameCharacter, lastName: lastNameCharacter },
-  skill: skillCharacter,
-  desc: describeSkill,
-} = myObject;
-
-console.log(`name: ${firstNameCharacter} ${lastNameCharacter}
-skill: ${skillCharacter}
-desc: ${describeSkill}`);
+button.addEventListener("click", function () {
+  document.body.style.backgroundColor = "silver";
+  button.style.backgroundColor = "yellow";
+  button.style.color = "red";
+});
