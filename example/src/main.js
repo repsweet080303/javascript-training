ecf7cfe695961d20674c436abd2d38f16f8eb890
PxViet <xuanviet0299@gmail.com>
2022-12-23 14:32:08 +0700

    #37 modified attributes in the DOM

--- example/src/main.js
+++ example/src/main.js
@@ -1,25 +1,13 @@
-const heading = document.querySelector("#heading");
-// const orderList = document.getElementsByTagName("ul");
-const listOrder = document.querySelector("ul");
-const paragraph = document.createElement("p");
-const newTodo = document.createElement("li");
-const modifiedTodo = document.createElement("li");
+const img = document.querySelector("img");
+const title = document.querySelector("title");
 
-heading.style.textTransform = "uppercase";
+console.log(title);
+title.innerText = "Learn the DOM";
 
-newTodo.innerText = "Read DOM manipulation";
-modifiedTodo.innerText = "Eat diet";
+console.log(img);
+console.log(img.hasAttribute("src"));
+console.log(img.getAttribute("src"));
 
-// listOrder.forEach((item) => (item.style.listStyle = "none"));
-listOrder.style.listStyle = "none";
-listOrder.style.padding = "0 10px";
-
-listOrder.appendChild(newTodo);
-listOrder.insertBefore(newTodo, listOrder.lastElementChild);
-listOrder.replaceChild(modifiedTodo, listOrder.children[2]);
-listOrder.children[2].remove();
-paragraph.innerText = "This is text include by inner text";
-console.log(paragraph);
-
-const text = document.createComment("This is comment");
-console.log(text);
+img.removeAttribute("src");
+img.setAttribute("src", "/kokurikozaka003.36ec5b0a.jpg");
+img.setAttribute("alt", "ghibli");
