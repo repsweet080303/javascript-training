12644f25b51d24b5fdeb0c7f200b53180c0e4bfd
PxViet <xuanviet0299@gmail.com>
2022-12-25 21:05:02 +0700

    #44 Exercise 2 part 5

--- example/src/main.js
+++ example/src/main.js
@@ -1,8 +1,7 @@
-const link = document.getElementById("w3r");
-const btn = document.querySelector("button");
-
-function getAttributes() {
-  console.log("link: " + link.getAttribute("href"));
-  console.log("language: " + link.getAttribute("hreflang"));
-  console.log("type: " + link.getAttribute("type"));
+function insert_Row() {
+  let table = document.getElementById("sampleTable").insertRow(2);
+  let cellOne = table.insertCell(0);
+  let cellTwo = table.insertCell(1);
+  cellOne.innerHTML = "New Cell1";
+  cellTwo.innerHTML = "New Cell2";
 }
