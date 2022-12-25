bc3c898f0631219eab5860f0947cec2d4a3da553
PxViet <xuanviet0299@gmail.com>
2022-12-25 18:24:00 +0700

    #40 practice exercise 2 part 1

--- example/src/main.js
+++ example/src/main.js
@@ -1,70 +1,7 @@
-var books = [
-  {
-    title: "The Design of EveryDay Things",
-    author: "Don Norman",
-    alreadyRead: false,
-  },
-  {
-    title: "The Most Human Human",
-    author: "Brian Christian",
-    alreadyRead: true,
-  },
-];
+const text = document.getElementById("text");
 
-const paragraph = document.createElement("p");
-
-// books.forEach((book) => {
-//   const paragraphDescripton = document.createTextNode(
-//     ` Book ${book.title} by ${book.author}
-//      `
-//   );
-//   paragraph.appendChild(paragraphDescripton);
-//   document.body.appendChild(paragraph);
-//   console.log(paragraph.textContent);
-// });
-
-// for (let i = 0; i < books.length; i++) {
-//   const paragraphDescription = document.createTextNode(
-//     `Book ${books[i].title} by ${books[i].author}
-//     `
-//   );
-//   console.log(paragraphDescription);
-//   paragraph.appendChild(paragraphDescription);
-//   console.log(paragraph.textContent);
-//   document.body.appendChild(paragraph);
-// }
-
-// ====================Bonus====================
-
-var books = [
-  {
-    title: "The Design of EveryDay Things",
-    img: "http://ecx.images-amazon.com/images/I/41j2ODGkJDL._AA115_.jpg",
-    author: "Don Norman",
-    alreadyRead: false,
-  },
-  {
-    title: "The Most Human Human",
-    img: "http://ecx.images-amazon.com/images/I/41Z56GwEv9L._AA115_.jpg",
-    author: "Brian Christian",
-    alreadyRead: true,
-  },
-];
-
-var bookList = document.createElement("ul");
-
-for (let i = 0; i < books.length; i++) {
-  let bookItem = document.createElement("li");
-  let bookImg = document.createElement("img");
-  bookImg.src = books[i].img;
-  bookItem.appendChild(bookImg);
-  const paragraphDescription = document.createTextNode(
-    `Book ${books[i].title} by ${books[i].author}`
-  );
-  bookItem.appendChild(paragraphDescription);
-  if (books[i].alreadyRead) {
-    bookItem.style.color = "grey";
-  }
-  bookList.appendChild(bookItem);
+function js_style() {
+  text.style.fontFamily = "Segoe UI";
+  text.style.color = "#737373";
+  text.style.fontSize = "18px";
 }
-document.body.appendChild(bookList);
