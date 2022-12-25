61b78414a107a1c7be924b2240b6c6a1ed090327
PxViet <xuanviet0299@gmail.com>
2022-12-25 20:42:02 +0700

    #43 Exercise 2 part 4

--- example/src/main.js
+++ example/src/main.js
@@ -1,12 +1,8 @@
-const paragraph = document.createElement("p");
-const btn = document.createElement("button");
+const link = document.getElementById("w3r");
+const btn = document.querySelector("button");
 
-paragraph.innerText = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est deserunt dolorum soluta reiciendis. Totam modi quisquam asperiores vitae, voluptate veniam qui sed sit quod sapiente, quia porro cumque, dolorum a?`;
-btn.innerText = "Change bg";
-
-document.body.appendChild(paragraph);
-document.body.appendChild(btn);
-
-btn.addEventListener("click", () => {
-  paragraph.style.backgroundColor = "#737373";
-});
+function getAttributes() {
+  console.log("link: " + link.getAttribute("href"));
+  console.log("language: " + link.getAttribute("hreflang"));
+  console.log("type: " + link.getAttribute("type"));
+}
