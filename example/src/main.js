dd790fac963b7ed0d87af039217a00ac1074442e
PxViet <xuanviet0299@gmail.com>
2022-12-26 14:45:41 +0700

    #48 Build HTML/CSS table and form pomodoro timer task

--- example/src/main.js
+++
@@ -1,8 +0,0 @@
-const myForm = document.getElementById("MyForm");
-const radius = document.getElementById("radius");
-
-myForm.addEventListener("submit", () => {
-  event.preventDefault();
-  let volume = document.getElementById("volume");
-  volume.value = ((4 / 3) * (Math.PI * radius.value ** 3)).toFixed(2);
-});
