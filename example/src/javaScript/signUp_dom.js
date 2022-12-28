06e06178de9c14d329023e9999409e5be668c4b0
PxViet <xuanviet0299@gmail.com>
2022-12-28 18:11:39 +0700

    #49 Validate Form

--- example/src/javaScript/signUp_dom.js
+++ example/src/javaScript/signUp_dom.js
@@ -1,31 +1,156 @@
 const body = document.querySelector("body");
-const blockForm = document.createElement("div");
-const blockItem = document.createElement("div");
-const headText = document.createElement("h1");
-const formSignUp = document.createElement("form");
-const messengerInvalid = document.createElement("p");
-const labelItem = document.createElement("label");
-const inputItem = document.createElement("input");
-const messengerValid = document.createElement("p");
+body.innerHTML = `<div class="form-block">
+      <form id="signUp">
+        <h1>Sign Up Page</h1>
+        <div class="form-value">
+          <p class="email-msg" id="error-msg"></p>
+          <label for="email">Email:</label>
+          <input
+            type="email"
+            id="email-input"
+            placeholder="username@somewhere.sth"
+          />
+        </div>
+        <div class="form-value">
+          <p class="user-msg" id="error-msg"></p>
+          <label for="user">Username:</label>
+          <input type="text" id="user" placeholder="user name" />
+        </div>
+        <div class="form-value">
+          <p class="password-msg" id="error-msg"></p>
+          <label for="password">Password:</label>
+          <input type="password" id="password-input" placeholder="password" />
+        </div>
+        <div class="form-value">
+          <p class="confirm-msg" id="error-msg"></p>
+          <label for="confirm">Confirm :</label>
+          <input
+            type="password"
+            id="confirm-password-input"
+            placeholder="confirm password"
+          />
+        </div>
 
-formSignUp.className = "form-sign-up";
-blockForm.className = "form-block";
-headText.innerText = "Sign Up Page";
-messengerInvalid.innerText = "Invalid";
-messengerInvalid.className = "mess-email";
-messengerInvalid.setAttribute("id", "error-msg");
-blockItem.className = "input-item";
-labelItem.setAttribute("for", "email");
-labelItem.innerText = "Email :";
-inputItem.setAttribute("type", "email");
-inputItem.setAttribute("id", "email");
-inputItem.setAttribute("placeholder", "username@somewhere.sth");
-messengerValid.innerText = "Valid";
+        <div class="button-group">
+          <button type="submit">Sign up</button>
+          <button type="reset">Reset</button>
+        </div>
 
-body.appendChild(blockForm);
-blockForm.appendChild(formSignUp);
-blockItem.appendChild(messengerInvalid);
-blockItem.appendChild(labelItem);
-blockItem.appendChild(inputItem);
-formSignUp.appendChild(headText);
-formSignUp.appendChild(blockItem);
+        <p class="form__info"></p>
+        <a href="#">Back to index page</a>
+      </form>
+    </div>`;
+
+const formSignUp = document.getElementById("signUp");
+const email = document.getElementById("email-input");
+const username = document.getElementById("user");
+const password = document.getElementById("password-input");
+const confirmPassword = document.getElementById("confirm-password-input");
+
+//validate Email
+const emailValidate = (input) => {
+  const messEmail = document.querySelector(".email-msg");
+  const validRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (input.value === "") {
+    messEmail.innerText = "Email is empty !";
+    return false;
+  } else if (validRegex.test(input.value)) {
+    messEmail.innerText = "";
+    return true;
+  } else {
+    console.log("here");
+    messEmail.innerText =
+      "Email address wrong format. example: username@somewhere.sth";
+    return false;
+  }
+};
+
+const usernameValidate = (username) => {
+  const messUser = document.querySelector(".user-msg");
+  const validRegex = /^[A-Za-z][A-Za-z0-9_]{7,30}$/;
+
+  if (validRegex.test(username.value)) {
+    messUser.innerText = "";
+    return true;
+  } else if (username.value === "") {
+    messUser.innerText = "Username is empty !";
+    return false;
+  } else if (username.value.length > 30) {
+    messUser.innerText = "Username too long, limit 30 characters";
+  } else {
+    messUser.innerText =
+      "Username is invalid. Be sure it does not contain strange symbols or extra spaces anywhere";
+    return false;
+  }
+};
+
+const passwordValidate = (password) => {
+  const messPassword = document.querySelector(".password-msg");
+  const validRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+  if (password.value === "") {
+    messPassword.innerText = "Password is empty !";
+    return false;
+  } else if (password.value.length < 8) {
+    messPassword.innerText = "Password to short, much longer than 8 characters";
+    return false;
+  } else if (validRegex.test(password.value)) {
+    messPassword.innerText = "";
+    return true;
+  } else {
+    messPassword.innerText =
+      "Password is invalid. It must contain letters and at least one digit";
+    return false;
+  }
+};
+
+const confirmPassValidate = (password, confirmPassword) => {
+  const messConfirm = document.querySelector(".confirm-msg");
+
+  if (confirmPassword.value === "") {
+    messConfirm.innerText = "Confirm Password is empty !";
+    return false;
+  } else if (password.value === confirmPassword.value) {
+    messConfirm.innerText = "";
+    return true;
+  } else {
+    messConfirm.innerText = "Password confirm isn't matches the password";
+    return false;
+  }
+};
+
+const validate = (event) => {
+  event.preventDefault();
+
+  const formInfo = document.querySelector(".form__info");
+  const emailValidation = emailValidate(email);
+  const usernameValidation = usernameValidate(username);
+  const passwordValidation = passwordValidate(password);
+  const passwordConfirmValidation = confirmPassValidate(
+    password,
+    confirmPassword
+  );
+
+  if (
+    emailValidation === true &&
+    usernameValidation === true &&
+    passwordValidation === true &&
+    confirmPassword === true
+  ) {
+    console.log("first");
+    formInfo.innerText = `Email: ${email.value}
+    Username: ${username.value}
+    Password: ${password.value}
+    Confirm: ${passwordConfirm.value}`;
+
+    formInfo.style.color = "#53aa53";
+  } else {
+    formInfo.innerText = `Invalid information`;
+    formInfo.style.color = "#ed4337";
+  }
+};
+
+console.log(formSignUp);
+
+formSignUp.addEventListener("submit", validate);
