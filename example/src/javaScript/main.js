681d85e831b5db6cad9d8d55e6b695afdf49716e
PxViet <xuanviet0299@gmail.com>
2023-01-03 14:49:11 +0700

    #52 bind this with arrow function

--- example/src/javaScript/main.js
+++ example/src/javaScript/main.js
@@ -4,18 +4,48 @@ this.lastName = "Thao";
 const myObject = {
   firstName: "Cam",
   lastName: "Thao",
-  getFullName() {
-    // firstName = "Hung";
-    // lastName = "Van";
+  getFullName: () => {
+    firstName = "Hung";
+    lastName = "Van";
     console.log(` ${this.firstName} ${this.lastName}`);
   },
 };
 
-myObject.getFullName(); // Cam Thao
+myObject.getFullName(); // Hung Van
 
 const myFunction = myObject.getFullName.bind(this);
 
-myFunction(); // Dan Thao
-// When not bind(this) => reference object dot it
-// When bind(this) => reference global object
+myFunction(); //Hung Van
+
+// When bind(this) in arrow function
+// this references value in block
+
+// =============================================
+
+// this.firstName = "Dan";
+// this.lastName = "Thao";
+
+// const myObject = {
+//   firstName: "Cam",
+//   lastName: "Thao",
+//   getFullName: () => {
+//     // firstName = "Hung";
+//     // lastName = "Van";
+//     console.log(` ${this.firstName} ${this.lastName}`);
+//   },
+// };
+
+// myObject.getFullName(); // Dan Thao
+
+// const myFunction = myObject.getFullName.bind(this);
+
+// myFunction(); //Dan Thao
+
+// When bind(this) in arrow function
+// this references global object
+
+// =============================================
+
+
+
 
